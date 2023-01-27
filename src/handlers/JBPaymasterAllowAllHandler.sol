426ab2d5ae7e86a02365acd3bd5cff530099f557
xBA5ED <83727748+xBA5ED@users.noreply.github.com>
2023-01-27 18:06:32 +0100

    Add allow all handler

---
+++ src/handlers/JBPaymasterAllowAllHandler.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "../JBPaymaster.sol";
+import "../interfaces/IJBPaymasterHandler.sol";
+
+contract JBPaymasterAllowAllHandler is IJBPaymasterHandler {
+    
+    //*********************************************************************//
+    // --------------------------- custom errors ------------------------- //
+    //*********************************************************************//
+    error INVALID_CONFIGURATION();
+
+    /**
+     * @notice
+     * @dev this should revert if its not allowed
+     */
+    function shouldAllowCall(
+        uint256,
+        address,
+        bytes4,
+        GsnTypes.RelayRequest calldata,
+        bytes calldata,
+        uint256 // maxPossibleGas
+    ) external view returns (bytes memory, bool) {
+        // We don't require a callback
+        return (bytes(""), false);
+    }
+
+    function postRelayCall(bytes memory context) external {}
+}
